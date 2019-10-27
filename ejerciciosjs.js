1402cf19eba1fcb2a32b5df62e945a6cac453ff7
fpezcara <fpezcara@gmail.com>
2019-10-27 09:51:42 +0000

    Ejercicios JS

---
+++ ejerciciosjs.js
@@ -0,0 +1,49 @@
+// Ejercicio 27
+let numero1 = 14;
+let numero2 = 8;
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+console.log(numero1 % numero2);
+
+// Ejercicio 28
+let numero = 9
+console.log(numero * 1);
+console.log(numero * 2);
+console.log(numero * 3);
+console.log(numero * 4);
+console.log(numero * 5);
+console.log(numero * 6);
+console.log(numero * 7);
+console.log(numero * 8);
+console.log(numero * 9);
+console.log(numero * 10);
+
+// Ejercicio 29
+let perimetroCuadrado = 10
+
+console.log(perimetroCuadrado * 4);
+
+// Ejercicio 30
+let areaCuadrado = 5
+
+console.log(areaCuadrado ** 2)
+
+// Ejercicio 31
+let ladoUno = 10;
+let ladoDos = 20;
+let ladoTres = 5;
+
+let perimetroTriangulo = (ladoUno + ladoDos + ladoTres);
+
+console.log(perimetroTriangulo);
+
+// Ejercicio 32
+let altura = 10;
+let base = 4;
+
+console.log((altura * 2) + (base * 2));
+
+console.log(base * altura);
